295176f08e8ed9847ee772f38820a438b2d44214
abadiamo <alba.badia@uab.cat>
2020-02-24 11:51:13 +0100

    first commit

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+class Calculator:
+   
+    urbag_constant = 12.4
+    
+    def sum(a,b):
+        return a+b
---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 24 11:37:25 2020
+
+@author: usuari
+"""
+# from file import class
+from Calculator import Calculator 
+
+result = Calculator.sum(3,4)
+
+print("The result is", result)
